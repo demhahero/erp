7c4eaeb9b74f6e20528f22fab2b7e7ff95535ae4
demhahero <demhahero@gmail.com>
2015-07-28 15:52:38 +0900

    init

---
+++ MVCempty/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCempty.Models;
+namespace MVCempty.Controllers
+{
+    public class HomeController : Controller
+    {
+        datalinqDataContext db = new datalinqDataContext();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<product> model = new List<product>();
+            model = db.products.ToList();
+            return View(model);
+        }
+    }
+}
---
+++ MVCempty/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCempty.Global" Language="C#" %>
